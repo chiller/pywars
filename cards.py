d2cb95a14887170b5bfd97b66c53db0030673fd8
Kelemen Kata <kelemenkata@gmail.com>
2015-05-02 11:48:59 +0000

    moved creature card functions out of Card, implemented building card.

--- cards.py
+++ cards.py
@@ -9,37 +9,16 @@ class Card(object):
     def __init__(self, field):
         self.field = field
         self.effects = [Effect(self)]
-
+    
+    def get_att(self):
+        return 0
+    
     def die(self):
         index = self.field.cards.index(self)
         self.field.cards[index] = EmptyField(self.field)
         self.field.player.discard_pile.append(self.__class__)
         for card in self.field.cards:
             card.effects.append(FriendlyHasDiedEffect(card))
-    
-    def get_bonus_hp(self):
-        return sum(map(lambda x:x.defense_modifier(),self.effects))
-
-    def get_hp(self):
-        return self.hp + \
-            self.get_bonus_hp() -\
-            self.hp_lost
-    
-    def get_att(self):
-        return self.att + sum(map(lambda x: x.attack_modifier(), self.effects))
-    
-    def get_hit(self, damage):
-        self.hp_lost += damage
-        if self.get_hp() <= 0:
-            self.die()
-
-    def attack(self, card):
-        card.get_hit(self.get_att())
-        self.get_hit(card.get_att())
-    
-    def __str__(self):
-        return "[%s%d/%d]" % (self.strname, self.get_att(), self.get_hp())
-
     
 class EmptyField(Card):
     hp = 0
@@ -57,7 +36,40 @@ class EmptyField(Card):
 
 
 class CreatureCard(Card):
-    pass
+    
+    
+    def __str__(self):
+        return "[%s%d/%d]" % (self.strname, self.get_att(), self.get_hp())
+
+    def get_hp(self):
+        return self.hp + \
+            self.get_bonus_hp() -\
+            self.hp_lost
+    
+    def get_att(self):
+        return self.att + sum(map(lambda x: x.attack_modifier(), self.effects))
+    
+    def get_hit(self, damage):
+        self.hp_lost += damage
+        if self.get_hp() <= 0:
+            self.die()
+
+    def attack(self, card):
+        card.get_hit(self.get_att())
+        self.get_hit(card.get_att())
+       
+    def get_bonus_hp(self):
+        sum_from_effects = sum(map(lambda x:x.defense_modifier(),self.effects))
+        if self.get_building():
+            from_building = self.get_building().bonus_hp
+        else:
+            from_building = 0
+        return from_building + sum_from_effects
+    
+    def get_building(self):
+        card_index = self.field.cards.index(self)
+        if self.field.cards[card_index]:
+            return self.field.buildings[card_index]
 
 class SpellCard(Card):
     def __str__(self):
@@ -87,4 +99,5 @@ class DrawCardsCard(SpellCard):
         self.effects = [DrawCardsEffect(self)]
 
 class CelestialCastle(BuildingCard):
-    pass
+    strname = "CC"
+    bonus_hp = 3
